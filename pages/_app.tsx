c9245f6e34036afa3257370f155c82895b626c94
0xcen <c3noriega@gmail.com>
2022-07-30 14:56:18 +0200

    course 4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,22 @@
 import { AppProps } from "next/app";
-// import Head from "next/head";
+import Head from "next/head";
 // import { ApolloProvider } from "@apollo/client";
 // import { useApollo } from "src/apollo";
 // import { AuthProvider } from "src/auth/useAuth";
-// import "../styles/index.css";
+import "../styles/index.css";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <title>Home sweet Home</title>
+        <link
+          rel="shortcut icon"
+          href="/images/favicon.ico"
+          type="image/x-icon"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
